ddc48610cdd98854cae146d1f05d586a70b2879f
GFBryson <gfb@gillianfbryson.ca>
2020-08-03 15:03:08 -0700

    rebasing from master and merging in changes to allow for overall accuracy testing

--- src/feature_effectiveness.py
+++ src/feature_effectiveness.py
@@ -1,13 +1,5 @@
 from os import path
 
-import json
-from operator import itemgetter
-from os import path
-
-import pandas as pd
-from sklearn import datasets
-
-import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
@@ -30,27 +22,27 @@ def main():
         avg_effectiveness = []
         for featureName in datasets[0]["feature_names"]:
             avg_effectiveness.append({featureName: 0})
-        #run multiple times to find an average
-        for a in range (0,100):
+        # run multiple times to find an average
+        for a in range(0, 100):
             X = dataset["X"]
             y = dataset["y"]
             test_percentages = dataset["test_percentage"]
             splitters = dataset["tree_splitters"]
 
-            #Keep the splits the same so test results are reliable
+            # Keep the splits the same so test results are reliable
             X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=1)
 
-            #this is the accuracy when no features have been removed
+            # this is the accuracy when no features have been removed
             full_percentage = test_percentage_test(X_train, y_train, X_test, y_test)
 
             features = dataset["feature_names"]
 
-            for a in range (0,len(features)):
+            for a in range(0, len(features)):
                 feature_dropped = features[a]
                 tempX = X.drop(features[a], 1)
                 X_train, X_test, y_train, y_test = train_test_split(tempX, y, test_size=0.5, random_state=1)
 
-                #this is the accuracy when a specific feature is removed (positive means accuracy got worse when removed, negative means accuracy got better when removed)
+                # this is the accuracy when a specific feature is removed (positive means accuracy got worse when removed, negative means accuracy got better when removed)
                 percentage = test_percentage_test(X_train, y_train, X_test, y_test)
 
                 relative_effectiveness = full_percentage[0.5] - percentage[0.5]
@@ -69,6 +61,10 @@ def main():
             scores.append(list(avg_effectiveness[c].values())[0]/100)
 
         plt.figure(figsize=(9,8))
+        features_string_commas = ', '.join(dataset['feature_names'])
+        features_string_dash = "-".join([x.split("_")[0] for x in dataset['feature_names']])
+
+        fig, ax = plt.subplots()
         plt.bar(objects, scores)
         plt.ylabel('Relative Accuracy')
         plt.xlabel('Feature')
@@ -76,6 +72,10 @@ def main():
         plt.title('Effectiveness of Features')
         plt.tight_layout()
         output_path = path.join(project_path, "..", f"./outputs/avg_accuracy_features_{dataset['tag']}")
+        plt.text(0, 1, f"features used: {features_string_commas}", wrap=True, transform=ax.transAxes,
+                 fontsize='xx-small')
+        output_path = path.join(project_path, "..", "outputs",
+                                f"avg_accuracy_features_{dataset['tag']}_{features_string_dash}")
         plt.savefig(output_path)
         plt.clf()
 
@@ -129,76 +129,6 @@ def get_accuracy(X_train, y_train, X_test, y_test):
 
     acc = metrics.accuracy_score(y_test, prediction)
     return acc
-
-def get_dataset():
-    X = []
-    y = []
-    project_path = path.abspath(path.dirname(__file__))
-    json_path = path.join(project_path, "..", "testing_data.json")
-    config = json.loads(open(json_path, "r").read())
-    keys, data_path, WK1_label, WK2_label, WK3_label, WK4_label, test_percentage, target_names, feature_names, layer_sizes, tree_splitters, forrest_splitters = itemgetter(
-        "keys",
-        "data_path",
-        "WK1_label",
-        "WK2_label",
-        "WK3_label",
-        "WK4_label",
-        "test_percentage",
-        "target_names",
-        "feature_names",
-        "layer_sizes",
-        "tree_splitters",
-        "forrest_splitters")(
-        config)
-
-    data_path = path.join(project_path, data_path)
-    frame = pd.read_csv(data_path, header=None, names=keys)
-
-    X = frame[feature_names]
-
-    return [
-        {
-            "tag": "WK1",
-            "X": X,
-            "y": frame[WK1_label],
-            "test_percentage": test_percentage,
-            "target_names": target_names,
-            "feature_names": feature_names,
-            "layer_sizes": layer_sizes,
-            "tree_splitters": tree_splitters,
-            "forrest_splitters": forrest_splitters
-        },{
-            "tag": "WK2",
-            "X": X,
-            "y": frame[WK2_label],
-            "test_percentage": test_percentage,
-            "target_names": target_names,
-            "feature_names": feature_names,
-            "layer_sizes": layer_sizes,
-            "tree_splitters": tree_splitters,
-            "forrest_splitters": forrest_splitters
-        },{
-            "tag": "WK3",
-            "X": X,
-            "y": frame[WK3_label],
-            "test_percentage": test_percentage,
-            "target_names": target_names,
-            "feature_names": feature_names,
-            "layer_sizes": layer_sizes,
-            "tree_splitters": tree_splitters,
-            "forrest_splitters": forrest_splitters
-        },{
-            "tag": "WK4",
-            "X": X,
-            "y": frame[WK4_label],
-            "test_percentage": test_percentage,
-            "target_names": target_names,
-            "feature_names": feature_names,
-            "layer_sizes": layer_sizes,
-            "tree_splitters": tree_splitters,
-            "forrest_splitters": forrest_splitters
-        }
-    ]
 
 
 if __name__ == '__main__':
